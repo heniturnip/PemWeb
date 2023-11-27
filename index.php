ebdfe39b3e00b14ad59d78e6c34aa2c0b5ebe2c2
Heni_121140080 <127072361+heniturnip@users.noreply.github.com>
2023-11-27 12:36:58 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+heni artha uli br turnip <br>
+121140080
+
+<!-- beranda.php -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Beranda</title>
+</head>
+
+<body>
+    <h1>Hallo! Welcome to My Web</h1>
+    <p> Didalam web terdapat Data Mahasiswa, Form Input Barang, dan Tulis file </p>
+    <ul>
+        <li><a href="showarray.php">Data Mahasiswa</a></li>
+        <li><a href="form.php">Form Input Barang</a></li>
+        <li><a href="tulis.php">Tulis File</a></li>
+    </ul>
+</body>
+</html>
